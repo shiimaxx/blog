6de4ea649bc3837c6da7aac4da5d2d9a3152a790
shiimaxx <yoshima@shiimaxx.com>
2018-02-28 22:41:11 +0900

    Add temporary implementation of login

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
