75f892de172f58aef8bd874f867d44fe58f862a6
shiimaxx <yoshima@shiimaxx.com>
2018-03-01 22:45:50 +0900

    Add custom user model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    qiita_id = models.CharField('Qiita ID', max_length=50, blank=True)
