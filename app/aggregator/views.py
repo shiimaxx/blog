50c18d34d662b2f94113b6b771c7c76ddb93cb88
shiimaxx <yoshima@shiimaxx.com>
2018-03-04 12:24:16 +0900

    Add some ViewSet

--- app/aggregator/views.py
+++ app/aggregator/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from accounts.models import User
+from aggregator.models import QiitaEntry
+from aggregator.serializer import UserSerializer, QiitaEntrySerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class QiitaEntryViewSet(viewsets.ModelViewSet):
+    queryset = QiitaEntry.objects.all()
+    serializer_class = QiitaEntrySerializer
