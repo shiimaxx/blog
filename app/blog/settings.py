dbcc135b5390e005ecf8cd711bbf83e672d9bcf4
shiimaxx <yoshima@shiimaxx.com>
2018-03-05 21:40:39 +0900

    Add blog to INSTALL_APPS

--- app/blog/settings.py
+++ app/blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
     'accounts',
     'aggregator',
     'rest_framework'
