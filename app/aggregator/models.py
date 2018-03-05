0a99f25a9d997589b93b17227b055b6d376c1ac0
shiimaxx <yoshima@shiimaxx.com>
2018-03-05 21:40:11 +0900

    Delete some fields from QiitaEntry model

--- app/aggregator/models.py
+++ app/aggregator/models.py
@@ -4,16 +4,8 @@ from accounts.models import User
 
 # Create your models here.
 class QiitaEntry(models.Model):
-    rendered_body = models.TextField()
-    body = models.TextField()
-    comments_count = models.PositiveIntegerField()
     created_at = models.DateTimeField()
     id = models.CharField(primary_key=True, max_length=128)
-    like_count = models.PositiveIntegerField()
-    private = models.BooleanField()
-    reactions_count = models.PositiveIntegerField()
     title = models.CharField(max_length=128)
-    updated_at = models.DateTimeField()
     url = models.URLField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    page_views_count = models.PositiveIntegerField()
