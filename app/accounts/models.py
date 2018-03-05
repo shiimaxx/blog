132177ed0e81089e50a63d8d4582d527a25c439e
shiimaxx <yoshima@shiimaxx.com>
2018-03-05 21:38:54 +0900

    Add qiita_id field and some methods to User model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -3,4 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    qiita_id = models.CharField(max_length=128, null=True)
+
+    @classmethod
+    def find_qiita_user(cls):
+        return cls.objects.filter(qiita_id__isnull=False)
+
+    @classmethod
+    def get_from_qiita_id(cls, qiita_id):
+        return cls.objects.filter(qiita_id=qiita_id).first()
