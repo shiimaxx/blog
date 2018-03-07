cb8d7bccadb153dd943bbe124dde28aec5edb648
shiimaxx <yoshima@shiimaxx.com>
2018-03-07 22:25:05 +0900

    Add limit for http method
    Refs: https://stackoverflow.com/questions/9549867/django-rest-framework-limit-the-allowed-methods-to-get

--- app/aggregator/views.py
+++ app/aggregator/views.py
@@ -10,10 +10,12 @@ from aggregator.serializer import UserSerializer, QiitaEntrySerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ['get', 'head', 'options']
 
 
 class QiitaEntryViewSet(viewsets.ModelViewSet):
     serializer_class = QiitaEntrySerializer
+    http_method_names = ['get', 'head', 'options']
 
     def get_queryset(self):
         return QiitaEntry.objects.filter(user=self.kwargs['user_pk'])
