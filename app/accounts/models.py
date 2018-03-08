e4066c6fe4200a4df54ed9d5a76ad1a65a8d2f31
shiimaxx <yoshima@shiimaxx.com>
2018-03-08 18:13:56 +0900

    Modify user id to uuid

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     qiita_id = models.CharField(unique=True, max_length=128, null=True)
 
     @classmethod
