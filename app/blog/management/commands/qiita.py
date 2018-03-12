f599700b32d429a369fa5e68110b9e2653455b23
shiimaxx <yoshima@shiimaxx.com>
2018-03-12 22:23:18 +0900

    Move some functions out Command class

--- app/blog/management/commands/qiita.py
+++ app/blog/management/commands/qiita.py
@@ -7,32 +7,32 @@ import json
 import requests
 
 
+def _fetch_qiita_entries(qiita_id):
+    base_url = 'https://qiita.com/api/v2'
+    endpoint = '{base_url}/users/{qiita_id}/items'.format(base_url=base_url, qiita_id=qiita_id)
+    response = requests.get(endpoint)
+    return json.loads(response.text)
+
+def save_qiita_entries(users):
+    for user in users:
+        entries_json = _fetch_qiita_entries(user.qiita_id)
+
+        entries = [
+            QiitaEntry(
+                id=entry_json['id'],
+                title=entry_json['title'],
+                url=entry_json['url'],
+                created_at=entry_json['created_at'],
+                user=User.get_from_qiita_id(entry_json['user']['id'])
+            )
+            for entry_json in entries_json
+            if not QiitaEntry.objects.filter(id=entry_json['id']).exists()
+        ]
+        QiitaEntry.objects.bulk_create(entries)
+
 class Command(BaseCommand):
     help = 'Fetch and save for qiita entries'
 
-    def _fetch_qiita_entries(self, qiita_id):
-        base_url = 'https://qiita.com/api/v2'
-        endpoint = '{base_url}/users/{qiita_id}/items'.format(base_url=base_url, qiita_id=qiita_id)
-        response = requests.get(endpoint)
-        return json.loads(response.text)
-
-    def save_qiita_entries(self, users):
-        for user in users:
-            entries_json = self._fetch_qiita_entries(user.qiita_id)
-
-            entries = [
-                QiitaEntry(
-                    id=entry_json['id'],
-                    title=entry_json['title'],
-                    url=entry_json['url'],
-                    created_at=entry_json['created_at'],
-                    user=User.get_from_qiita_id(entry_json['user']['id'])
-                )
-                for entry_json in entries_json
-                if not QiitaEntry.objects.filter(id=entry_json['id']).exists()
-            ]
-            QiitaEntry.objects.bulk_create(entries)
-
     def handle(self, *args, **options):
         users = User.find_qiita_user()
-        self.save_qiita_entries(users)
+        save_qiita_entries(users)
