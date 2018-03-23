1bbcf8df4fb32889512a7983994e7b2a5bd96c9b
shiimaxx <yoshima@shiimaxx.com>
2018-03-23 19:46:46 +0900

    Add post, delete test to blogentry api

--- app/blog/tests/test_blogentry_api.py
+++ app/blog/tests/test_blogentry_api.py
@@ -29,28 +29,28 @@ class BlogEntryApiTests(APITestCase):
         def add_timezone(created_at):
             return created_at.astimezone(timezone('Asia/Tokyo'))
 
-        BlogEntry.objects.create(
+        self.dummy_entry1 = BlogEntry.objects.create(
             title='dummy_entry1',
             content='dummy_content1',
             created_at=add_timezone(datetime(2018, 1, 1, 0, 0)),
             category=self.dummy_category1,
             user=self.dummy_user1
         )
-        BlogEntry.objects.create(
+        self.dummy_entry2 =  BlogEntry.objects.create(
             title='dummy_entry2',
             content='dummy_content2',
             created_at=add_timezone(datetime(2018, 1, 5, 10, 0)),
             category=self.dummy_category2,
             user=self.dummy_user1
         )
-        BlogEntry.objects.create(
+        self.dummy_entry3 = BlogEntry.objects.create(
             title='dummy_entry3',
             content='dummy_content3',
             created_at=add_timezone(datetime(2018, 2, 11, 7, 20)),
             category=self.dummy_category1,
             user=self.dummy_user2
         )
-        BlogEntry.objects.create(
+        self.dummy_entry4 = BlogEntry.objects.create(
             title='dummy_entry4',
             content='dummy_content4',
             created_at=add_timezone(datetime(2018, 2, 20, 14, 15)),
@@ -71,6 +71,31 @@ class BlogEntryApiTests(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 0)
 
+    def test_blog_entry_post(self):
+        entry = {
+            'title': 'dummy_entry5',
+            'content': 'dummy_content5',
+            'created_at': '2018-03-01T00:00:00+09:00',
+            'category': '{}'.format(self.dummy_category1.id),
+            'user': '{}'.format(self.dummy_user1.id)
+        }
+        response = self.client.post('/api/v1/users/{}/blogentries/'.format(self.dummy_user1.id), data=entry)
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/api/v1/users/{}/blogentries/'.format(self.dummy_user1.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 3)
+
+    def test_blog_entry_delete(self):
+        response = self.client.delete(
+            '/api/v1/users/{}/blogentries/{}/'.format(
+                self.dummy_user1.id, self.dummy_entry1.id
+            )
+        )
+        self.assertEqual(response.status_code, 204)
+        response = self.client.get('/api/v1/users/{}/blogentries/'.format(self.dummy_user1.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
     def test_method_not_allowd(self):
         entry = {
             'title': 'dummy_entry5',
@@ -79,11 +104,6 @@ class BlogEntryApiTests(APITestCase):
             'category': '{"name": "dummy_category1"}',
             'user': '{"id": "dummy_qiita_user1"}'
         }
-        response = self.client.post('/api/v1/users/{}/blogentries/'.format(self.dummy_user1.id), data=entry)
-        self.assertEqual(response.status_code, 405)
 
         response = self.client.put('/api/v1/users/{}/blogentries/'.format(self.dummy_user1.id), data=entry)
-        self.assertEqual(response.status_code, 405)
-
-        response = self.client.delete('/api/v1/users/{}/blogentries/dummy_id1/'.format(self.dummy_user1.id))
         self.assertEqual(response.status_code, 405)
