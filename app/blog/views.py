e011a028961d71c0bda2a21aef0e1c9eeb0cf3c9
shiimaxx <yoshima@shiimaxx.com>
2018-03-23 19:31:40 +0900

    Update http_method_names for BlogEntryViewSet

--- app/blog/views.py
+++ app/blog/views.py
@@ -6,7 +6,7 @@ from blog.serializer import BlogEntrySerializer
 
 class BlogEntryViewSet(viewsets.ModelViewSet):
     serializer_class = BlogEntrySerializer
-    http_method_names = ['get', 'head', 'options']
+    http_method_names = ['get', 'head', 'options', 'post', 'delete']
 
     def get_queryset(self):
         return BlogEntry.objects.filter(user=self.kwargs['user_pk'])
