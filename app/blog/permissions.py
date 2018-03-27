16132757a1e1e71debc89f8be4e69459899607af
shiimaxx <yoshima@shiimaxx.com>
2018-03-27 22:33:48 +0900

    Add has_permission to blog/permission.py

--- app/blog/permissions.py
+++ app/blog/permissions.py
@@ -2,6 +2,12 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
