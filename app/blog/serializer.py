579429d573d35b90db9238a3a4016755aa5b36ad
shiimaxx <yoshima@shiimaxx.com>
2018-03-28 22:22:30 +0900

    Return username for BlogEntrySerializer

--- app/blog/serializer.py
+++ app/blog/serializer.py
@@ -4,6 +4,11 @@ from blog.models import BlogEntry
 
 
 class BlogEntrySerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = BlogEntry
         fields = ('id', 'title', 'content', 'created_at', 'category', 'user')
+
+    def get_user(self, obj):
+        return str(obj.user.username)
